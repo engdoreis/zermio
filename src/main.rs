70fde060ac5589408a13ca91e51e9567f8245dda
Douglas Reis <engdoreis@gmail.com>
2025-05-12 12:30:01 +0100

    Rector the cli arguments to be extensible
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use reis_mmiogen::generator;
-use reis_mmiogen::schema;
+use reismmiolib::generator;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 use std::str::FromStr;
 
@@ -9,35 +8,69 @@ use quick_xml::de::from_reader;
 use std::fs::File;
 use std::io::BufReader;
 
+#[derive(Subcommand, Debug)]
+enum Input {
+    InportSvd {
+        /// A path to a svd file
+        #[arg(long, short, value_parser =  PathBuf::from_str)]
+        svd: PathBuf,
+        #[command(subcommand)]
+        output: Output,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum Output {
+    ExportCpp {
+        /// A dir to output the peripheral implementation.
+        #[arg(long, short, value_parser =  PathBuf::from_str)]
+        peripheral_dir: PathBuf,
+
+        /// A dir to output the header with the peripheral addresses.
+        #[arg(long, short, value_parser =  PathBuf::from_str)]
+        addresses_dir: Option<PathBuf>,
+    },
+}
+
 // Define a struct to represent command-line options
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// A path to a svd file
-    #[arg(long, value_parser =  PathBuf::from_str)]
-    svd: PathBuf,
-
-    /// A path to the output dir
-    #[arg(long, value_parser =  PathBuf::from_str)]
-    output: PathBuf,
+    #[command(subcommand)]
+    input: Input,
 }
 
 fn main() -> anyhow::Result<(), &'static str> {
     let args: Args = Args::parse();
-    if !args.svd.exists() {
-        return Err("Svd does not exist!");
+
+    let (device, output) = match args.input {
+        Input::InportSvd { svd, output } => {
+            if !svd.exists() {
+                return Err("Svd does not exist!");
+            }
+            println!("Loading the {}...", svd.display());
+            let file = File::open(svd).unwrap();
+            let reader = BufReader::new(file);
+            (from_reader(reader).unwrap(), output)
+        }
+    };
+
+    match output {
+        Output::ExportCpp {
+            peripheral_dir,
+            addresses_dir,
+        } => {
+            if !peripheral_dir.is_dir() {
+                return Err("Output path is not a dir!");
+            }
+            let addresses_dir = addresses_dir.unwrap_or(peripheral_dir.clone());
+            if !addresses_dir.is_dir() {
+                return Err("Addresses path is not a dir!");
+            }
+
+            generator::cpp::generate(&device, peripheral_dir, addresses_dir).unwrap();
+        }
     }
-    if !args.output.is_dir() {
-        return Err("Output path is not a dir!");
-    }
 
-    println!("Loading xml");
-
-    let file = File::open(args.svd).unwrap();
-    let reader = BufReader::new(file);
-
-    let device: schema::Device = from_reader(reader).unwrap();
-    // println!("{:#?}", device);
-    generator::cpp::generate(&device, args.output).unwrap();
     Ok(())
 }
