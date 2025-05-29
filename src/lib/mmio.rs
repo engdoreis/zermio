a6e97552814c384e62648b231e34bd4ca3757a21
Douglas Reis <engdoreis@gmail.com>
2025-05-29 21:09:43 +0100

    [Refactor] Use crates svd_parser and svd_rs to ingest svd
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/lib/mmio.rs
+++ src/lib/mmio.rs
@@ -2,13 +2,14 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::schema;
-#[derive(Debug, Clone, strum::Display, strum::IntoStaticStr)]
-#[strum(serialize_all = "PascalCase")]
+#[derive(Debug, Clone, Default, strum::Display, strum::IntoStaticStr)]
 pub enum Permissions {
     Read,
     Write,
+    #[default]
     ReadWrite,
+    ReadWriteOnce,
+    WriteOnce,
 }
 
 impl From<&str> for Permissions {
@@ -18,6 +19,18 @@ impl From<&str> for Permissions {
             "write-only" => Self::Write,
             "read-write" => Self::ReadWrite,
             _ => panic!("{} unsuported", s),
+        }
+    }
+}
+
+impl From<svd_rs::access::Access> for Permissions {
+    fn from(s: svd_rs::access::Access) -> Self {
+        match s {
+            svd_rs::access::Access::ReadOnly => Permissions::Read,
+            svd_rs::access::Access::ReadWrite => Permissions::ReadWrite,
+            svd_rs::access::Access::ReadWriteOnce => Permissions::ReadWriteOnce,
+            svd_rs::access::Access::WriteOnce => Permissions::WriteOnce,
+            svd_rs::access::Access::WriteOnly => Permissions::Write,
         }
     }
 }
@@ -36,22 +49,17 @@ impl<'a> Device<'a> {
     }
 }
 
-pub struct Register<'a> {
-    pub name: &'a str,
-    pub desc: &'a str,
+pub struct Register {
+    pub name: String,
+    pub desc: String,
     pub offset: String,
     pub bitfields: Vec<Bitfield>,
 }
 
-impl<'a> Register<'a> {
-    pub fn new(
-        name: &'a str,
-        offset: u32,
-        desc: Option<&'a str>,
-        bitfields: Vec<Bitfield>,
-    ) -> Self {
+impl Register {
+    pub fn new(name: String, offset: u32, desc: Option<String>, bitfields: Vec<Bitfield>) -> Self {
         Self {
-            name,
+            name: name.clone(),
             desc: desc.unwrap_or(name),
             offset: format!("{:#x}", offset),
             bitfields,
@@ -67,15 +75,45 @@ pub struct Bitfield {
     pub permissions: Permissions,
 }
 
-impl From<&schema::Field> for Bitfield {
-    fn from(reg: &schema::Field) -> Self {
+impl Bitfield {
+    fn new(
+        name: String,
+        desc: String,
+        bit_size: u32,
+        offset: u32,
+        permissions: Permissions,
+    ) -> Self {
         Self {
-            name: reg.name.clone(),
-            offset: reg.bit_range.offset as u32,
-            bit_size: reg.bit_range.size as u32,
-            permissions: reg.access.clone(),
-            desc: reg.description.clone(),
+            name,
+            offset,
+            bit_size,
+            permissions,
+            desc,
         }
+    }
+}
+
+impl Default for Bitfield {
+    fn default() -> Self {
+        Self::new(
+            "value".to_string(),
+            "value".to_string(),
+            32,
+            0,
+            Permissions::default(),
+        )
+    }
+}
+
+impl From<&svd_rs::field::FieldInfo> for Bitfield {
+    fn from(field: &svd_rs::field::FieldInfo) -> Self {
+        Self::new(
+            field.name.clone(),
+            field.description.clone().unwrap_or(field.name.clone()),
+            field.bit_range.width,
+            field.bit_range.offset,
+            Permissions::from(field.access.clone().unwrap_or_default()),
+        )
     }
 }
 
@@ -89,9 +127,29 @@ pub struct DeviceTypes {
     pub devices: Vec<DeviceAddr>,
 }
 
+#[derive(Debug, Clone)]
+pub struct Interrupt {
+    pub name: String,
+    pub description: String,
+    pub value: u32,
+}
+
+impl From<&svd_rs::Interrupt> for Interrupt {
+    fn from(interrupt: &svd_rs::Interrupt) -> Self {
+        Self {
+            name: interrupt.name.clone(),
+            description: interrupt
+                .description
+                .clone()
+                .unwrap_or(interrupt.name.clone()),
+            value: interrupt.value,
+        }
+    }
+}
+
 pub struct Platform {
     pub device_types: Vec<DeviceTypes>,
-    pub interrupts: Vec<schema::Interrupt>,
+    pub interrupts: Vec<Interrupt>,
 }
 
 impl Platform {
@@ -121,7 +179,7 @@ impl Platform {
         }
     }
 
-    pub fn add_interrupt(&mut self, interrupt: schema::Interrupt) {
+    pub fn add_interrupt(&mut self, interrupt: Interrupt) {
         self.interrupts.push(interrupt);
     }
 }
