12e435486b2f73c80e437e3a7de691d577dfe220
Douglas Reis <engdoreis@gmail.com>
2025-05-30 13:40:37 +0100

    [Refactor] Move the translation to mmio.rs
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- src/lib/mmio.rs
+++ src/lib/mmio.rs
@@ -2,13 +2,19 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+pub fn device_type(s: &str) -> String {
+    let re = regex::Regex::new(r"\d+$").unwrap();
+    // Remove trailing digits
+    re.replace(s, "").to_string().to_lowercase()
+}
+
 #[derive(Debug, Clone, Default, strum::Display, strum::IntoStaticStr)]
 pub enum Permissions {
-    Read,
-    Write,
     #[default]
     ReadWrite,
     ReadWriteOnce,
+    Read,
+    Write,
     WriteOnce,
 }
 
@@ -35,20 +41,6 @@ impl From<svd_rs::access::Access> for Permissions {
     }
 }
 
-pub struct Device<'a> {
-    pub name: &'a str,
-    pub registers: Vec<&'a str>,
-}
-
-impl<'a> Device<'a> {
-    pub fn new(name: &'a str) -> Self {
-        Self {
-            name,
-            registers: vec![],
-        }
-    }
-}
-
 pub struct Register {
     pub name: String,
     pub desc: String,
@@ -57,7 +49,13 @@ pub struct Register {
 }
 
 impl Register {
-    pub fn new(name: String, offset: u32, desc: Option<String>, bitfields: Vec<Bitfield>) -> Self {
+    pub fn new(
+        name: impl Into<String>,
+        offset: u32,
+        desc: Option<String>,
+        bitfields: Vec<Bitfield>,
+    ) -> Self {
+        let name = name.into();
         Self {
             name: name.clone(),
             desc: desc.unwrap_or(name),
@@ -77,18 +75,18 @@ pub struct Bitfield {
 
 impl Bitfield {
     fn new(
-        name: String,
-        desc: String,
+        name: impl Into<String>,
+        desc: impl Into<String>,
         bit_size: u32,
         offset: u32,
         permissions: Permissions,
     ) -> Self {
         Self {
-            name,
+            name: name.into(),
+            desc: desc.into(),
             offset,
             bit_size,
             permissions,
-            desc,
         }
     }
 }
@@ -116,7 +114,35 @@ impl From<&svd_rs::field::FieldInfo> for Bitfield {
         )
     }
 }
+impl TryFrom<&svd_rs::field::Field> for Bitfield {
+    type Error = String;
+    fn try_from(field: &svd_rs::field::Field) -> Result<Self, Self::Error> {
+        Ok(Self::from(match field {
+            svd_rs::field::Field::Single(info) => info,
+            svd_rs::field::Field::Array(_, _) => {
+                return Err("Field Array not supported".to_string());
+            }
+        }))
+    }
+}
 
+pub struct Device {
+    pub name: String,
+    pub type_: String,
+    pub registers: Vec<Register>,
+}
+
+impl Device {
+    pub fn new(name: impl Into<String>, type_name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            type_: type_name.into(),
+            registers: vec![],
+        }
+    }
+}
+
+// Platform specific
 pub struct DeviceAddr {
     pub name: String,
     pub address: String,
@@ -148,18 +174,15 @@ impl From<&svd_rs::Interrupt> for Interrupt {
 }
 
 pub struct Platform {
+    pub name: String,
     pub device_types: Vec<DeviceTypes>,
     pub interrupts: Vec<Interrupt>,
+    //Define the number of data bit-width of the maximum single data transfer supported by the bus infrastructure
+    pub bus_width: u32,
+    pub devices: Vec<Device>,
 }
 
 impl Platform {
-    pub fn new() -> Self {
-        Self {
-            device_types: Vec::new(),
-            interrupts: Vec::new(),
-        }
-    }
-
     pub fn add_register(&mut self, type_name: String, device_name: String, address: u64) {
         let new_device = DeviceAddr {
             name: device_name,
@@ -178,14 +201,97 @@ impl Platform {
             });
         }
     }
-
-    pub fn add_interrupt(&mut self, interrupt: Interrupt) {
-        self.interrupts.push(interrupt);
-    }
 }
 
-impl Default for Platform {
-    fn default() -> Self {
-        Self::new()
+impl TryFrom<svd_rs::device::Device> for Platform {
+    type Error = String;
+    fn try_from(svd_device: svd_rs::device::Device) -> Result<Self, Self::Error> {
+        let mut this = Self {
+            name: svd_device.name,
+            device_types: Vec::new(),
+            interrupts: Vec::new(),
+            bus_width: svd_device.width,
+            devices: Vec::new(),
+        };
+
+        for periph in &svd_device.peripherals {
+            let device_iter = match periph {
+                svd_rs::peripheral::Peripheral::Single(info) => info,
+                svd_rs::peripheral::Peripheral::Array(_, _) => {
+                    return Err("Peripheral array not supported".to_string());
+                }
+            };
+
+            // i.e 0x8000_0000
+            let device_addr = device_iter.base_address;
+            // i.e UART0
+            let device_name = device_iter.name.replace(" ", "_").to_uppercase();
+            // i.e UART
+            let device_type =
+                device_type(device_iter.derived_from.as_ref().unwrap_or(&device_name));
+
+            this.add_register(device_type.clone(), device_name.clone(), device_addr);
+
+            for interrupt in &device_iter.interrupt {
+                let mut interrupt: Interrupt = interrupt.into();
+                interrupt.name = format!("{}_{}", device_name, interrupt.name);
+                this.interrupts.push(interrupt);
+            }
+
+            let Some(ref registers) = device_iter.registers else {
+                continue;
+            };
+
+            let mut device = Device::new(&device_name, &device_type);
+            for register_cluster in registers {
+                let register = match register_cluster {
+                    svd_rs::registercluster::RegisterCluster::Register(register) => register,
+                    svd_rs::registercluster::RegisterCluster::Cluster(_) => {
+                        println!(
+                            "Warning: Register cluster not supported in peripheral {}, skipping.",
+                            device_name
+                        );
+                        continue;
+                    }
+                };
+
+                let register_iter = match register {
+                    svd_rs::register::Register::Single(info) => info,
+                    svd_rs::register::Register::Array(_, _) => {
+                        println!(
+                            "Warning: Register Array not supported in peripheral {}, skipping.",
+                            device_name
+                        );
+                        continue;
+                    }
+                };
+
+                let bitfields = if let Some(ref bitfields) = register_iter.fields {
+                    bitfields
+                        .iter()
+                        .map(|field| {
+                            field.try_into().unwrap_or_else(|e| {
+                                panic!("{} {}::{}", e, device_name, register_iter.name)
+                            })
+                        })
+                        .collect::<Vec<Bitfield>>()
+                } else {
+                    let mut default = Bitfield::default();
+                    default.bit_size = svd_device.width;
+                    vec![default]
+                };
+
+                let register = Register::new(
+                    register_iter.name.clone(),
+                    register_iter.address_offset as u32,
+                    register_iter.description.clone(),
+                    bitfields,
+                );
+
+                device.registers.push(register);
+            }
+            this.devices.push(device);
+        }
+        Ok(this)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -40,29 +40,30 @@ struct Args {
     input: Input,
 }
 
-fn main() -> anyhow::Result<(), &'static str> {
+fn main() -> anyhow::Result<(), String> {
     let args: Args = Args::parse();
 
     let (device, output) = match args.input {
         Input::ImportSvd { svd, output } => {
             if !svd.exists() {
-                return Err("Svd does not exist!");
+                return Err("Svd does not exist!".to_string());
             }
 
             println!("Loading the {}...", svd.display());
             let xml = std::fs::read_to_string(&svd).unwrap();
-            (svd_parser::parse(&xml).unwrap(), output)
+            let svd_rs = svd_parser::parse(&xml).unwrap().try_into()?;
+            (svd_rs, output)
         }
     };
 
     match output {
         Output::ExportCpp { dir, periph_dir } => {
             if !dir.is_dir() {
-                return Err("Output path is not a dir!");
+                return Err("Output path is not a dir!".to_string());
             }
             let periph_dir = periph_dir.unwrap_or(dir.clone());
             if !periph_dir.is_dir() {
-                return Err("Addresses path is not a dir!");
+                return Err("Addresses path is not a dir!".to_string());
             }
 
             generator::cpp::generate(&device, dir, periph_dir).unwrap();
