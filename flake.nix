b8ad16144ff6b5d869cbdcf4bdee492a2f278d1f
Douglas Reis <engdoreis@gmail.com>
2025-06-04 20:01:27 +0100

    [nix] Package in the flake for easy use in other flakes
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,17 +1,49 @@
 {
-    description = "C/C++ environment";
+  description = "C/C++ environment";
 
-    inputs = {
-         nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-         flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        # This is a derivation to be used downstream.
+        zermio-cli = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "zermio-cli";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = "${src}/Cargo.lock";
+          };
+
+          doCheck = false;
+          meta = {
+            description = "A CLI tool to generate mmio code from hardware descripition languages";
+            homepage = "https://github.com/engdoreis/zermio";
+          };
+        };
+      in {
+        formatter = pkgs.alejandra;
+        packages.default = zermio-cli;
+        apps.default = flake-utils.lib.mkApp {
+          drv = zermio-cli;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rustup
