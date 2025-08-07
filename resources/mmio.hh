da7cffdcbc9a6df2f9941f1894c11a9ff7149941
Douglas Reis <engdoreis@gmail.com>
2025-08-07 20:41:51 +0100

    [cpp] Update license header of mmio.hh
    Signed-off-by: Douglas Reis <engdoreis@gmail.com>

--- resources/mmio.hh
+++ resources/mmio.hh
@@ -2,6 +2,10 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+/*
+ * This file was copied over from zermio repository.
+ */
+
 #pragma once
 
 #include <cstdint>
