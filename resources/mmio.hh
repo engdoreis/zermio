0b916909a2a20118b40511701f3867efcd1bc055
Douglas Reis <doreis@lowrisc.org>
2026-01-03 16:53:27 +0000

    [C++] Fix function write_mask

--- resources/mmio.hh
+++ resources/mmio.hh
@@ -79,10 +79,10 @@ namespace zermio {
         return *this;
       }
 
-      inline constexpr auto& bit_mask(std::size_t value, std::size_t offset) {
+      inline constexpr auto& write_mask(std::size_t value, std::size_t offset) {
         static_assert(BITS > 1, ">> Error: This bitfield is multibit. Try using set or reset. <<");
         static_assert(Writable<P>, ">> Error: This bitfield can't be write. <<");
-        reg.cache &= ~((0x01 << (OFFSET+offset)) & mask());
+        reg.cache &= ~((value << (OFFSET+offset)) & mask());
         reg.cache |= ((value << (OFFSET+offset)) & mask());
         return *this;
       }
