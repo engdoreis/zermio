e8beaf6111ef5655b27fe33aae6d61fbf0fd9088
Douglas Reis <doreis@lowrisc.org>
2026-02-08 09:54:43 +0000

    [Lint] Add reuse to lint licenses

--- resources/zermio.rs
+++ resources/zermio.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Douglas Reis.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
 
 use core::cmp::PartialEq;
 use core::ops::{
